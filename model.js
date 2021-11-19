3c6cf6587467e534307c983e9ecc32092e5b8184
Muhammad <mamadmuhammad832@gmail.com>
2021-11-19 22:07:46 +0700

    Initial Commit

--- model.js
+++ model.js
@@ -10,8 +10,35 @@ function loadData() {
 }
 
 // Buat fungsi untuk load data disini
+async function loadModel(){
+  console.log('loading model...');
+  model = await tf.loadModel(
+    'file://${__dirname}/model/model.json',
+    false
+  );
+  console.log('Model Load Successful')
+};
 
 const movie_arr = tf.tensor(loadData());
 const movie_len = movies.length;
 
 // Buat fungsi untuk memberi rekomendasi film
+
+exports.recommend = async function recommend(userId){
+  let user = tf.fill([movie_len], Number(userId));
+  let movie_in_js_array = movie_arr.arraySync();
+  await loadModel();
+  console.log('Recommending for User: ${userId}');
+  pred_tensor = await model.predict([movie_arr, user]).reshape([movie_len]);
+
+  let recommendations = [];
+  for(let i = 0; i < 6; i++){
+    max = pred_tensor.argMax().arraySync();
+    recommendations.push(movies[max]);
+    pred.splice(max,1);
+    pred_tensor = tf.tensor(pred);
+  }
+
+  console.log(recommendations);
+  return recommendations;
+}
